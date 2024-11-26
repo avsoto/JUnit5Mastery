d0a6f7111f36e23b3dcc01de40f2abe16a28f629
avsotomejia@gmail.com <avsotomejia@gmail.com>
2024-11-26 00:01:56 -0500

    Finishing the metestRelationBetweenBankAndAccounts()

--- src/main/java/org/avsoto/junit/model/Account.java
+++ src/main/java/org/avsoto/junit/model/Account.java
@@ -3,6 +3,7 @@ package org.avsoto.junit.model;
 import java.math.BigDecimal;
 import java.util.UUID;
 import lombok.*;
+import org.avsoto.junit.exceptions.InsufficientBalanceException;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,6 +17,7 @@ public class Account {
     private String firstName;
     private String lastName;
     private BigDecimal balance;
+    private Bank bank;
 
     @Override
     public boolean equals(Object obj) {
@@ -33,10 +35,14 @@ public class Account {
     }
 
     public void debit(BigDecimal amount){
-
+        BigDecimal newBalance = this.balance.subtract(amount);
+        if(newBalance.compareTo(BigDecimal.ZERO) < 0){
+            throw new InsufficientBalanceException("Insufficient Balance.");
+        }
+        this.balance = newBalance;
     }
 
     public void credit(BigDecimal amount){
-
+        this.balance = this.balance.add(amount);
     }
 }
--- src/test/java/org/avsoto/junit/model/AccountTest.java
+++ src/test/java/org/avsoto/junit/model/AccountTest.java
@@ -1,4 +1,5 @@
 package org.avsoto.junit.model;
+import org.avsoto.junit.exceptions.InsufficientBalanceException;
 import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import static org.junit.jupiter.api.Assertions.*;
@@ -81,8 +82,107 @@ class AccountTest {
                 .build();
         account.debit(new BigDecimal("100"));
         assertNotNull(account.getBalance());
-        assertNotEquals(900, account.getBalance().intValue());
-        assertEquals("1000.12345", account.getBalance().toPlainString());
+        assertNotEquals(800, account.getBalance().intValue());
+        assertEquals("900.12345", account.getBalance().toPlainString());
 
+    }
+
+    @Test
+    void testCreditAccount() {
+        Account account = Account.builder()
+                .id(2)
+                .firstName("Andrés Daniel")
+                .lastName("Michelena Erchkat")
+                .balance(new BigDecimal("1000.12345"))
+                .build();
+        account.credit(new BigDecimal("100"));
+        assertNotNull(account.getBalance());
+        assertNotEquals(900, account.getBalance().intValue());
+        assertEquals("1100.12345", account.getBalance().toPlainString());
+
+    }
+
+    @Test
+    void testToEnsureInsufficientBalance() {
+        Account account = Account.builder()
+                .id(3)
+                .firstName("Andrés Daniel")
+                .lastName("Michelena Erchkat")
+                .balance(new BigDecimal("1000.12345"))
+                .build();
+        //Assert to handle exceptions
+        Exception exception = assertThrows(InsufficientBalanceException.class, () -> {
+           account.debit(new BigDecimal(1500));
+        });
+
+        String actualMessage = exception.getMessage();
+        String expectedMessage = "Insufficient Balance.";
+
+        assertEquals(actualMessage, expectedMessage);
+    }
+
+    @Test
+    void testTransferAmountBetweenAccounts() {
+        Account account = Account.builder()
+                .id(5)
+                .firstName("Yulissa Linda")
+                .lastName("Sevilla Lopez")
+                .balance(new BigDecimal("2500"))
+                .build();
+        Account accountTwo = Account.builder()
+                .id(6)
+                .firstName("Andres Daniel")
+                .lastName("Doe Mirto")
+                .balance(new BigDecimal("1500.8989"))
+                .build();
+
+        Bank bank = Bank.builder()
+                .name("BBVA")
+                .build();
+
+        bank.transfer(accountTwo, account, new BigDecimal(500));
+        assertEquals("1000.8989", accountTwo.getBalance().toPlainString());
+        assertEquals("3000", account.getBalance().toPlainString());
+
+    }
+
+    @Test
+    void testRelationBetweenBankAndAccounts() {
+        Account account = Account.builder()
+                .id(5)
+                .firstName("Yulissa Linda")
+                .lastName("Sevilla Lopez")
+                .balance(new BigDecimal("2500"))
+                .build();
+        Account accountTwo = Account.builder()
+                .id(6)
+                .firstName("Andres Daniel")
+                .lastName("Doe Mirto")
+                .balance(new BigDecimal("1500.8989"))
+                .build();
+
+        Bank bank = new Bank();
+        bank.addAccount(account);
+        bank.addAccount(accountTwo);
+
+        bank.setName("BBVA");
+
+        bank.transfer(accountTwo, account, new BigDecimal(500));
+        assertEquals("1000.8989", accountTwo.getBalance().toPlainString());
+        assertEquals("3000", account.getBalance().toPlainString());
+
+        assertEquals(2, bank.getAccounts().size());
+        assertEquals("BBVA", account.getBank().getName());
+        assertEquals("Andres Daniel", bank.getAccounts()
+                .stream()
+                .filter(c -> c.getFirstName()
+                        .equals("Andres Daniel"))
+                .findFirst()
+                .get().getFirstName());
+
+        assertTrue(bank.getAccounts()
+                .stream()
+                .anyMatch(c -> c.getFirstName()
+                        .equals("Andres Daniel")));
     }
 }
