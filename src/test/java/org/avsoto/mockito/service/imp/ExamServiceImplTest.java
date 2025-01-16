92e6fa18c4f9f522652a668782bea19d0dd232cd
avsoto <avsotomejia@gmail.com>
2025-01-16 18:43:57 -0500

    add: verify in tests

--- src/test/java/org/avsoto/mockito/service/imp/ExamServiceImplTest.java
+++ src/test/java/org/avsoto/mockito/service/imp/ExamServiceImplTest.java
@@ -1,33 +1,37 @@
 package org.avsoto.mockito.service.imp;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import org.avsoto.mockito.model.Exam;
 import org.avsoto.mockito.repository.ExamRepository;
-import org.avsoto.mockito.repository.ExamRepositoryOther;
 import org.avsoto.mockito.repository.QuestionRepository;
-import org.avsoto.mockito.service.ExamService;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class ExamServiceImplTest {
 
+    @Mock
     ExamRepository repository;
-    ExamService service;
+
+    @Mock
     QuestionRepository questionRepository;
+
+    @InjectMocks
+    ExamServiceImpl service;
 
     @BeforeEach
     void setUp() {
-        repository = mock(ExamRepository.class);
-        questionRepository = mock(QuestionRepository.class);
-        service = new ExamServiceImpl(repository, questionRepository);
+
     }
 
     @Test
@@ -58,10 +62,37 @@ class ExamServiceImplTest {
         when(repository.findAll()).thenReturn(Data.EXAMS);
         when(questionRepository.findQuestionsByExamId(5L)).thenReturn(Data.QUESTIONS);
 
-        Exam examn = service.findExamByNameAndQuestions("Maths");
+        Exam exam = service.findExamByNameAndQuestions("Maths");
 
-        assertEquals(3, examn.getQuestions().size());
-        assertTrue(examn.getQuestions().contains("Arithmetic"));
+        assertEquals(3, exam.getQuestions().size());
+        assertTrue(exam.getQuestions().contains("Arithmetic"));
+    }
+
+    @Test
+    void testQuestionsExamVerify(){
+        when(repository.findAll()).thenReturn(Data.EXAMS);
+        when(questionRepository.findQuestionsByExamId(5L)).thenReturn(Data.QUESTIONS);
+
+        Exam exam = service.findExamByNameAndQuestions("Maths");
+
+        assertEquals(3, exam.getQuestions().size());
+        assertTrue(exam.getQuestions().contains("Arithmetic"));
+
+        verify(repository).findAll();
+        verify(questionRepository).findQuestionsByExamId(5L);
+    }
+
+    @Test
+    void testNoExitsExamVerify(){
+        when(repository.findAll()).thenReturn(Collections.emptyList());
+        when(questionRepository.findQuestionsByExamId(anyLong())).thenReturn(Data.QUESTIONS);
+
+        Exam exam = service.findExamByNameAndQuestions("Maths");
+
+        assertNull(exam);
+
+        verify(repository).findAll();
+        verify(questionRepository).findQuestionsByExamId(5L);
     }
 
 
